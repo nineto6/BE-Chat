a2c28a6d5dc5e4724a746b2ab4e06af1c734d593
chang-seop <ikavon@naver.com>
2023-06-16 15:32:34 +0900

    ADD: RedisConfig, RedisRepository, RedisConfig
    BE-Login 프로젝트에서 가져오기

---
+++ chat/src/main/java/hello/chat/config/redis/RefreshToken.java
@@ -0,0 +1,27 @@
+package hello.chat.config.redis;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(value = "refresh", timeToLive = 259200) // 만료기간 3일로 지정
+public class RefreshToken {
+    @Id // null 로 저장될 경우 랜덤 값으로 설정됨
+    private String id;
+
+    private String ip;
+
+    @Indexed // 보조 인덱스 적용 (로그아웃시 필요)
+    private String userId;
+
+    @Indexed // Secondary indexes(보조 인덱스) 적용
+    private String refreshToken;
+}
