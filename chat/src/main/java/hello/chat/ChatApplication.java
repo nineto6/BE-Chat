a04c6405a775f258fc224f205637ce86c5c8ada3
chang-seop <ikavon@naver.com>
2023-06-16 15:13:41 +0900

    UPDATE: ssl-local.properties, application.properties, ChatApplication
    SSL 추가, application.properties 설정

--- chat/src/main/java/hello/chat/ChatApplication.java
+++ chat/src/main/java/hello/chat/ChatApplication.java
@@ -2,8 +2,10 @@ package hello.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:ssl-local.properties")
 public class ChatApplication {
 
 	public static void main(String[] args) {
