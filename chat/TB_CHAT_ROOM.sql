5bf15ebb57ef882378d48eced646bc00a46ffe95
chang-seop <ikavon@naver.com>
2023-06-24 23:07:48 +0900

    ADD: TB_CHAT_ROOM.sql

---
+++ chat/TB_CHAT_ROOM.sql
@@ -0,0 +1,8 @@
+drop table tb_chat_room cascade;
+
+create table tb_chat_room(
+   chat_room_sq       INT AUTO_INCREMENT PRIMARY KEY,
+   channel_id         VARCHAR(20) NOT NULL,
+   writer_nm          VARCHAR(20) NOT NULL,
+   date_time          TIMESTAMP NOT NULL
+);
