f91f32d2ee8aff86c2fd009fda5e152f4a2e1291
chang-seop <ikavon@naver.com>
2023-06-25 17:49:19 +0900

    UPDATE: TB_CHAT_ROOM.sql

--- chat/TB_CHAT_ROOM.sql
+++ chat/TB_CHAT_ROOM.sql
@@ -2,8 +2,9 @@ drop table tb_chat_room cascade;
 
 create table tb_chat_room(
    chat_room_sq       INT AUTO_INCREMENT PRIMARY KEY,
-   channel_id         VARCHAR(20) NOT NULL,
-   title              VARCHAR(20) NOT NULL,
+   channel_id         VARCHAR(36) NOT NULL,
+   title              VARCHAR(30) NOT NULL,
+   writer_id          VARCHAR(20) NOT NULL,
    writer_nm          VARCHAR(20) NOT NULL,
    date_time          TIMESTAMP NOT NULL
 );
