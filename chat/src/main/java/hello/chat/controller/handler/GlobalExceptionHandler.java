47599ee2d2255a943e0b71910ae6ab4387e06dc2
chang-seop <ikavon@naver.com>
2023-07-11 21:54:02 +0900

    UPDATE: GlobalExceptionHandler

--- chat/src/main/java/hello/chat/controller/handler/GlobalExceptionHandler.java
+++ chat/src/main/java/hello/chat/controller/handler/GlobalExceptionHandler.java
@@ -5,12 +5,32 @@ import hello.chat.config.exception.BusinessExceptionHandler;
 import hello.chat.controller.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    /**
+     * validation 전용 ExceptionHandler
+     * JSON -> 객체로 바인딩 시 Validation 을 진행 후 바인딩 실패 시 반환되는 Exception
+     * @param e
+     * @return ResponseEntity<ApiResponse>
+     */
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ApiResponse> validException(final MethodArgumentNotValidException e) {
+        log.info("valid Exception Handling Stack Trace : ", e);
+
+        ApiResponse ar = ApiResponse.builder()
+                .result("")
+                .resultCode(ErrorCode.BAD_REQUEST.getStatus())
+                .resultMsg(ErrorCode.BAD_REQUEST.getMessage())
+                .build();
+
+        return ResponseEntity.ok().body(ar);
+    }
 
     @ExceptionHandler(BusinessExceptionHandler.class)
     public ResponseEntity<ApiResponse> handleBusinessException(final RuntimeException e) {
